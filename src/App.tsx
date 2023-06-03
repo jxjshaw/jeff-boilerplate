80b7f80d510845ba3e3df33a6370e9f8bf46935c
Jeff Shaw <jxjshaw@gmail.com>
2023-06-03 11:44:56 -0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./index.css";
+import styled from "styled-components"
+
+const PageContainer = styled.div`
+  padding: 48px;
+`
+
+export default function App() {
+  return (
+    <PageContainer>
+        <h2> Template </h2>
+        <p> Template. </p>
+    </PageContainer>
+  );
+}
